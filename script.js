b0f5be50c0bd3daac82754957cc8b73375ca8eb8
Pratik Fuse <pratikfuse20@gmail.com>
2023-10-11 18:29:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 // complete this js code
-function Person(name, age) {}
+function Person(name, age) {
+}
 
 function Employee(name, age, jobTitle) {}
 
+Person.prototype = function greet(){
+	console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`)
+}
+
+Employee.prototype = function jobGreet(){
+	console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
+} 
 // Do not change code below this line
 window.Person = Person;
 window.Employee = Employee;
+
