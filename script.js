26d9a758f6931ac9bd96a7589b84d4d6e3c7fc24
Pratik Fuse <pratikfuse20@gmail.com>
2023-10-11 18:35:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ function Person(name, age) {
 
 function Employee(name, age, jobTitle) {}
 
-Person.prototype = function greet(){
+Person.prototype.greet= function(){
 	console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`)
 }
 
-Employee.prototype = function jobGreet(){
+Employee.prototype.jobGreet = function(){
 	console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
 } 
 // Do not change code below this line
