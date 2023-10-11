76ab355e82c53e3a513a66acd7cc4ce08f50ffa1
Pratik Fuse <pratikfuse20@gmail.com>
2023-10-11 18:38:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 // complete this js code
 function Person(name, age) {
+	this.name  = name;
+	this.age  = age;
 }
 
-function Employee(name, age, jobTitle) {}
+function Employee(name, age, jobTitle) {
+	this.name  = name;
+	this.age = age;
+	this.jobTitle = jobTitle;
+}
 
 Person.prototype.greet= function(){
 	console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`)
